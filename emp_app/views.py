cb38bac3e7ce4b18799e2fdac5a18b0621d565e8
neenu <neenujayesh874@gmail.com>
2026-01-20 00:29:13 +0530

    added changes

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,1 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from json import loads
+from emp_app.models import Employee
+
+@method_decorator(csrf_exempt,name="dispatch")
+class EmployeeListCreateView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        qs=Employee.objects.all()
+
+        result=[{"name":e.name,"department":e.department,"salary":e.salary,"location":e.location,"experience":e.experience,"age":e.age,"email":e.email,"phone":e.phone} for e in qs]
+
+        return JsonResponse({"data":result,"status":"200 ok"})
+
+    def post(self,request,*args,**kwargs):
+
+        data=loads(request.body)
+
+        Employee.objects.create(name=data.get("name"),department=data.get("department"),salary=data.get("salary"),location=data.get("location"),experience=data.get("experience"),age=data.get("age"),email=data.get("email"),phone=data.get("phone"))
+
+        return JsonResponse({"data":"record inserted","status":"201 created"})
+
+        
--- employee_log/urls.py
+++ employee_log/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from emp_app.views import EmployeeListCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('employees/',EmployeeListCreateView.as_view())
 ]
